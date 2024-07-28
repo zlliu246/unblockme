57de0c7380cb6823440daaf938311ab32a8cacd9
zlliu246 <zlliu246@gmail.com>
2024-07-28 19:43:24 +0800

    minor update

--- src/unblockme/__init__.py
+++ src/unblockme/__init__.py
@@ -4,9 +4,33 @@ from .state_utils import (
 )
 from .djikstra_utils import dijkstra_search
 
-def unblockme(state_raw: str):
-    state = create_state(state_raw)
+def unblockme(
+    state: str,
+    animate: bool =True
+) -> list[tuple[str]]:
+    """
+    Args:
+        state_raw (str): a string representing a puzzle eg.
+
+            state = '''
+            AAAB  
+            CDDBEE
+            CXXB F
+            C GHHF
+            G IF
+            JJJ I
+            '''
+
+        animate (bool): if True, the solution will be animated
+
+    Returns:
+        list of states representing solution
+
+    """
+    state = create_state(state)
     states = dijkstra_search(state)
-    animate_states(states)
+    if animate:
+        animate_states(states)
+    return states
 
 __all__ = ['unblockme']
